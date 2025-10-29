5b8a130a2256ebfbf6d5d262d676d61b5b96361f
alanDD-6 <alandvladimir@gmail.com>
2025-10-28 20:02:54 -0400

    alanDD-6.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/alanDD-6.py
@@ -0,0 +1,30 @@
+# https://python.org/
+
+# comentario en esta sola linea 
+
+"""
+esto es un comentario 
+de varias lineas
+con AlanD
+"""
+
+''' esto tmb cuenta
+como comentario de 
+vairas lineas'''
+
+
+my_variable = "vale veinte"
+my_variable = "ahora es quince"
+
+MY_CONSTANT = "es trece" # por convencion
+
+my_int = 6
+my_float = 23.32
+bool = True
+bool = False
+
+string = "un texto"
+other_string = 'otro tipo de texto'
+
+print("hola mundo")
+print(type(my_int))
